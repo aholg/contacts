7550db62bd2f1ef026a12ea7ca8c6d89d7a5a08b
aholg <holgersson31@hotmail.com>
2022-05-21 15:43:16 +0200

    Add main

---
+++ src/main/scala/aholg/solution/Main.scala
@@ -0,0 +1,18 @@
+package aholg.solution
+
+import akka.actor.typed.{ActorSystem, Scheduler}
+import akka.http.scaladsl.Http
+
+import scala.concurrent.ExecutionContextExecutor
+
+object Main extends App {
+  implicit val system: ActorSystem[ContactsActor.ContactsCommand] = ActorSystem(ContactsActor(), "ContactsActor")
+  implicit val executionContext: ExecutionContextExecutor = system.executionContext
+  implicit val scheduler: Scheduler = system.scheduler
+
+  val contactsActor = new ContactsActor(system)
+  val contactsService = new ContactsService(contactsActor)
+  val contactsController = new ContactsController(contactsService)
+
+  val bindingFuture = Http().newServerAt("localhost", 8080).bind(contactsController.route)
+}
